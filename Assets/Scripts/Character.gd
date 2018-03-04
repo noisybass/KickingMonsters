20110977ea928cf271a566209e3cb2463d9f3f7d
NoisyBass <laura.dcs4@gmail.com>
2018-03-04 23:40:39 +0000

    Basic player state machine

--- Assets/Scripts/Character.gd
+++ Assets/Scripts/Character.gd
@@ -7,7 +7,39 @@ export var max_speed = 200
 var speed = 0.0
 var velocity = Vector2()
 
+enum CharacterState { IDLE, MOVE }
+var state = null
+
+func _ready():
+	_change_state(CharacterState.IDLE)
+
+func _change_state(new_state):
+	
+	match new_state:
+		IDLE:
+			$AnimationPlayer.play('idle')
+		MOVE:
+			$AnimationPlayer.play('run')
+			
+	state = new_state
+
 func _physics_process(delta):
+	update_direction()
+	
+	if state == IDLE and input_direction:
+		_change_state(MOVE)
+	elif state == MOVE:
+		move()
+		if not input_direction:
+			_change_state(IDLE)
+
+
+func update_direction():
+	if input_direction:
+		last_move_direction = input_direction
+	
+	
+func move():
 	if input_direction:
 		last_move_direction = input_direction
 		speed = max_speed
